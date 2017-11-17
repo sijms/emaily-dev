a7695c6512a125f2dd75316a671bf93c991c5f18
Samy Sultan <samyibraheem@gmail.com>
2017-11-17 17:03:33 +0300

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    googleClientID: '909006399897-qnb5lc73mdk2jj3jj41v4m9ntt0fpvvr.apps.googleusercontent.com',
-    googleClientSecret: 'wXq_lIHQJc_5X65slnnfvHAH',
-    mongoURI: 'mongodb://samy:password@ds259255.mlab.com:59255/db-emaily-dev',
-    cookieKey: 'sadfkjidhekmmchnndbetgfafgosskljhdhuegy',
-    stripePublishableKey: 'pk_test_nxPcH3RbvfZL8hxbNY9DQp1N',
-    stripeSecretKey: 'sk_test_Q9NTDxq0Pumxhn64IswYQhpB'
-};
